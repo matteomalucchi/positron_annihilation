9ca742b85460911f5a6edc5f2508b471560a775c
Matteo Malucchi <matteomalucchi13@gmail.com>
2022-02-27 10:49:50 +0100

    modified plot

--- plot.cpp
+++ plot.cpp
@@ -11,24 +11,23 @@ void plot (){
     myfile.open("data/wave0_co_1.txt", ios::in | ios::out);  
     vector <double> t(1030);
     vector <vector<double>> v;
+    iota(begin(t), end(t), 0);
 
     if (myfile.is_open()){
         string tp;
         vector <double> w;
 
-        int i=-1, k=0, m, j=0;
+        int k=0, m, j=0;
 
         while(getline(myfile, tp)){ //read data from file object and put it into string.
             if (isalpha(tp[0]) == 0) {
                 if (k>0) {
                     k=0;
-                    i++;
                     w.clear();
                 }
                 m = stod(tp);
                 w.push_back(m);
                 j++;
-
             }
             else {
                 if (j>0) {
@@ -38,14 +37,10 @@ void plot (){
                 k++;
             }
         }
-        //non prendo l'ultimo evetno erchè è incompleto
+        //non prendo l'ultimo evetno perchè è incompleto
         myfile.close(); //close the file object.
         cout<< v[v.size()-1][v[v.size()-1].size()-1] <<endl;
-        iota(begin(t), end(t), 0);
-        /*
-        for (int j=0; j<1030; j++){
-            cout << t[j]<<endl;
-        }*/
+
     }    
     TCanvas *c1 = new TCanvas("c1","A Simple Graph Example");
     TGraph* gr = new TGraph(t.size(), &t[0], &v[0][0]);
